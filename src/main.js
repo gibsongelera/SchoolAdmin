02a51fc9c040591e4351e4c0a34e46fa1d85de82
Gibson Gelera <76902828+gibsongelera@users.noreply.github.com>
2024-04-05 23:56:13 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+$('.collapse').collapse()
 
+signUpButton.addEventListener('click', () =>
+container.classList.add('right-panel-active'));
+
+signInButton.addEventListener('click', () =>
+container.classList.remove('right-panel-active'));
